682379582fbeddda583e3f6aa1ce62a386df8df4
Terence Dixon <terencep.dixon@gmail.com>
2022-08-29 10:46:51 -0700

    commit

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,5 +1,13 @@
 import {ACTIONS} from './App'
 
 export default function OperationButton({dispatch, operation}){
-    return <button onClick={() => dispatch({type:ACTIONS.CHOOSE_OPERATION, payload: { operation }})}>{digit}</button>
+    return (
+        <button 
+        onClick={() => 
+            dispatch({type:ACTIONS.CHOOSE_OPERATION, payload: { operation }})
+            
+            }>     
+            {operation}
+            </button>
+    )
 }
