af4de56d4164fe1883e69a69bb6522afeb02318d
Terence Dixon <terencep.dixon@gmail.com>
2022-08-29 01:49:48 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {useReducer} from 'react'
 import './App.css';
+import './Components/styles.css'
+import DigitButton from './DigitButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//All the actions the user can do
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  EVALUATE: 'evaluate'
+}
+//takes in our state - different types of actions and the parameters that they take in
+function reducer(state, {type, payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        // spread syntax - takes in an iterable(usually an arrary),
+        //  and expands it into individual elements.
+        // commonly create copies of JS Objects
+        ...state,
+        currentOperand: `${state.currentOperand}${payload.digit}`
+      }
+  }
 }
 
+function App(){
+  //reducer
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <OperationButton digit="/" dispatch={dispatch}/>
+        <DigitButton digit="1" dispatch={dispatch}/>
+        <DigitButton digit="2" dispatch={dispatch}/>
+        <DigitButton digit="3" dispatch={dispatch}/>
+        <OperationButton digit="*" dispatch={dispatch}/>
+        <DigitButton digit="4" dispatch={dispatch}/>
+        <DigitButton digit="5" dispatch={dispatch}/>
+        <DigitButton digit="6" dispatch={dispatch}/> 
+        <OperationButton digit="+" dispatch={dispatch}/>
+        <DigitButton digit="7" dispatch={dispatch}/>
+        <DigitButton digit="8" dispatch={dispatch}/>
+        <DigitButton digit="9" dispatch={dispatch}/>
+        <OperationButton digit="-" dispatch={dispatch}/>
+        <DigitButton digit="." dispatch={dispatch}/>
+        <DigitButton digit="0" dispatch={dispatch}/>
+        <button className="span-two">=</button>
+    </div>
+  )
+}
 export default App;
